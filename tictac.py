e98aa329186229898c26f55a2b13d93a15202093
Ramon Barajas <ramonb621@yahoo.com>
2020-06-12 20:24:47 -0700

    edited read me, added images to mark down...

--- tictac.py
+++ tictac.py
@@ -6,11 +6,11 @@ def game_board(board):
     OUTPUT: Current gameboard
     '''
 
-    print(board[7]+"|"+board[8]+"|"+board[9])
-    print("-----")
-    print(board[4]+"|"+board[5]+"|"+board[6])
-    print("-----")
-    print(board[1]+"|"+board[2]+"|"+board[3])
+    print(' ' + board[7]+"|"+board[8]+"|"+board[9])
+    print(" -----")
+    print(' ' + board[4]+"|"+board[5]+"|"+board[6])
+    print(" -----")
+    print(' ' + board[1]+"|"+board[2]+"|"+board[3])
 
 # test_board = ['#','X','O','X','O','X','O','X','O','X']
 # game_board(test_board)
